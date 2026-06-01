b2a178f9dd6a14de754517e15eb802bd9a38f94e
umizu-commits <mission.mizumizu@gmail.com>
2026-06-01 16:37:54 +0900

    chore: CSPヘッダーを設定する

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,24 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
+Rails.application.configure do
+  config.content_security_policy do |policy|
+     policy.default_src :self
+     policy.font_src    :self
+     policy.img_src     :self, :data
+     policy.object_src  :none
+     policy.script_src  :self
+     policy.style_src   :self, :unsafe_inline
+     policy.connect_src :self
+     policy.form_action :self
 #     # Specify URI for violation reports
 #     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
+end
 #
 #   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
+   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+   config.content_security_policy_nonce_directives = %w(script-src)
 #
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
-# end
+end
