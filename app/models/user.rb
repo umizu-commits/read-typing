5e04d614c9845524ad3188a79da55939982af626
umizu-commits <mission.mizumizu@gmail.com>
2026-06-05 15:18:59 +0900

    feat: アカウント確認機能を一時的に無効化する

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         #:confirmable # 独自ドメイン設定後に再有効化する
+  # :confirmable # 独自ドメイン設定後に再有効化する
 end
