6b0f2e8859cc4d7cc014edd7b53ed8452855890d
umizu-commits <mission.mizumizu@gmail.com>
2026-06-05 14:37:50 +0900

    fix: lintチェック修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :resend
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
-    host: ENV["RENDER_EXTERNAL_HOSTNAME"], 
+    host: ENV["RENDER_EXTERNAL_HOSTNAME"],
     protocol: "https"
   }
 
