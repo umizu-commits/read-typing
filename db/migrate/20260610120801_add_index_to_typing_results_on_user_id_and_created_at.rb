64fa6437e2896c997d11ea3bed913a6225b707ad
umizu-commits <mission.mizumizu@gmail.com>
2026-06-10 21:32:15 +0900

    style: RuboCop の指摘に対応する

--- db/migrate/20260610120801_add_index_to_typing_results_on_user_id_and_created_at.rb
+++ db/migrate/20260610120801_add_index_to_typing_results_on_user_id_and_created_at.rb
@@ -1,5 +1,5 @@
 class AddIndexToTypingResultsOnUserIdAndCreatedAt < ActiveRecord::Migration[8.1]
   def change
-    add_index :typing_results, [:user_id, :created_at]
+    add_index :typing_results, [ :user_id, :created_at ]
   end
 end
