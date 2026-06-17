9405b1313adfef200c056242e9eece8d31be55de
umizu-commits <mission.mizumizu@gmail.com>
2026-06-17 15:08:49 +0900

    feat: タイピング履歴詳細画面を実装する

--- app/controllers/typing_histories_controller.rb
+++ app/controllers/typing_histories_controller.rb
@@ -7,4 +7,22 @@ class TypingHistoriesController < ApplicationController
     # ページネーション適用（テーブル表示用）
     @typing_results = @all_typing_results.recent.page(params[:page]).per(10)
   end
+
+  def show
+    @typing_result = TypingResult.find(params[:id])
+    authorize @typing_result
+  end
+
+  def update
+    @typing_result = TypingResult.find(params[:id])
+    authorize @typing_result
+
+    article = @typing_result.article
+    if article&.update(title: params.permit(:title)[:title])
+      redirect_to typing_history_path(@typing_result), notice: "記事タイトルを更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :show, status: :unprocessable_entity
+    end
+  end
 end
