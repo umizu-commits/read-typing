821d39c4fa9c4bad764021c54402e59d74a37574
umizu-commits <mission.mizumizu@gmail.com>
2026-06-22 21:50:29 +0900

    fix: Tagモデルのバリデーション重複を修正し、テキストフォームのUI順序を調整する

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,5 @@ class Tag < ApplicationRecord
   has_many :article_tags, dependent: :destroy
   has_many :articles, through: :article_tags
 
-  validates :name, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
